18a2d53e856d1356ef92f05b76af81d71e5ca764
SELVAKUMAR <selvakumar@SELVAKUMARs-MacBook-Pro-2.local>
2026-05-21 22:06:20 +0530

    selvam

--- frontend/src/constraint/constraint.js
+++ frontend/src/constraint/constraint.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://localhost:8000';
-//export const BASE_URL = 'https://enrollment.tnexams.net';
+//export const BASE_URL = 'http://localhost:8000';
+export const BASE_URL = 'https://enrollment.tnexams.net';
 // export const BASE_URL = 'https://osms.svnimaging.com';
 // export const BASE_URL = 'https://exam.obcrights.org/';
 // export const BASE_URL = 'https://dems.srmist.edu.in';
